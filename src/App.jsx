f915cb3967b8f454e07b829d9e64e6683a207f93
mdabdulkyum1 <kyummdabdul@gmail.com>
2024-12-11 11:38:06 +0600

    fix issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,8 @@
 import Navbar from './pages/Navbar';
 import ClickEffect from './components/ClickEffect';
 import Bubble from './components/Bubble';
-import Services from './components/Services/Services';
 
 function App() {
-
-  const me = {
-    name:"Md Abdul Kyum", 
-    age: 26,
-  }
-
-
-
-
 
 
   return (
@@ -20,7 +10,6 @@ function App() {
        <Bubble />
        <ClickEffect />
        <Navbar />
-       <Services me={me}></Services>
     </>
   );
 } 
