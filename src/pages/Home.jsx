00d7d46e73c6cda0850bfd8766b61641f837e2fd
mdabdulkyum1 <kyummdabdul@gmail.com>
2024-12-11 12:05:47 +0600

    audio added when scroll

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,12 +3,30 @@ import About from './../components/About';
 import Portfolio from '../components/Portfolio';
 import ContactForm from './../components/ContactForm';
 import Footer from './../components/Footer';
-
+import { useEffect, useRef } from 'react';
+import relax from '../assets/audio/relax.mp3'
 
 
 const Home = () => {
+
+  const audioRef = useRef(null);
+
+  useEffect(()=> {
+    const handelScroll = () => {
+      if(audioRef.current){
+          audioRef.current.play();
+      }
+    }
+
+    window.addEventListener('scroll', handelScroll);
+    return ()=> {
+      window.removeEventListener('scroll', handelScroll);
+    }
+  },[])
+
     return(
         <>
+          <audio ref={audioRef} src={relax} />
           <Banner />
           <About />
           <Portfolio />
